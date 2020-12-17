d1fde19870fa448e967c8c3c596b1886a0d5b17f
ctezna <ctezna@gmail.com>
2020-12-17 13:01:23 -0500

    update

--- project/sql/docCollection-hive.sql
+++ project/sql/docCollection-hive.sql
@@ -2,26 +2,6 @@ CREATE DATABASE DocCollection;
 
 USE DocCollection;
 
---- source 1 ----
-CREATE EXTERNAL TABLE DocMetadataSource1 (pubdate STRING, year INT, month INT, author STRING,
-                                    title STRING, section STRING, publication STRING) 
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
-STORED AS TEXTFILE 
-LOCATION 's3://ctezna-docstore/preprocessed/metadata/source1/';
-
-SELECT * FROM DocMetadataSource1 LIMIT 10;
-
-SELECT DISTINCT year
-FROM DocMetadataSource1
-WHERE year IS NOT NULL;
-
-SELECT year, COUNT(*)
-FROM DocMetadataSource1
-WHERE year IS NOT NULL
-GROUP BY year;
-
-SELECT count(DISTINCT publication)
-FROM DocMetadataSource1;
 
 --- source 2 ------
 
@@ -29,7 +9,7 @@ CREATE EXTERNAL TABLE DocMetadataSource2 (title STRING, publication STRING, auth
                                     pubdate STRING, year INT, month INT) 
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
 STORED AS TEXTFILE 
-LOCATION 's3://ctezna-docstore/preprocessed/metadata/source2/';
+LOCATION 's3://ctezna-docstore/preprocessed/metadata/articles/';
 
 SELECT * FROM docmetadatasource2 LIMIT 10;
 
@@ -52,23 +32,12 @@ CREATE EXTERNAL TABLE DocMetadataSource3 (author STRING, claps STRING, reading_t
                                     title STRING) 
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
 STORED AS TEXTFILE 
-LOCATION 's3://ctezna-docstore/preprocessed/metadata/source3/';
+LOCATION 's3://ctezna-docstore/preprocessed/metadata/blogposts/';
 
 SELECT * FROM docmetadatasource3 LIMIT 10;
 
 SELECT title, reading_time FROM docmetadatasource3
 ORDER BY reading_time DESC LIMIT 10;
-
---- source 5 ---
-
-CREATE EXTERNAL TABLE DocMetadataSource5 (author STRING, title STRING, highlighttext STRING,
-                                    language STRING, published DATE, crawled DATE) 
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
-STORED AS TEXTFILE 
-LOCATION 's3://ctezna-docstore/preprocessed/metadata/source5/';
-
-SELECT * FROM docmetadatasource5 LIMIT 10;
-
 --- all docs ----
 
 CREATE EXTERNAL TABLE Docs (text STRING) 
